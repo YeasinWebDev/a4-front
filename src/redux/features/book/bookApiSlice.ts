f914db63663e20aab38b9a0aba86e361a40f7f2b
ar9485727@gmail.com <ar9485727@gmail.com>
2025-07-04 14:26:06 +0600

    env added

--- src/redux/features/book/bookApiSlice.ts
+++ src/redux/features/book/bookApiSlice.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const bookApiSlice = createApi({
   reducerPath: "bookApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3000" }),
+  baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BACKENDURL }),
     tagTypes: ["Books"],
   endpoints: (builder) => ({
     books: builder.query({
--- src/redux/features/borrow/borrowApiSlice.ts
+++ src/redux/features/borrow/borrowApiSlice.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const borrowApiSlice = createApi({
   reducerPath: "borrowApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3000" }),
+  baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BACKENDURL}),
   tagTypes: ["Borrows"],
   endpoints: (builder) => ({
     createBorrow: builder.mutation({
